af76dda1edeff2a5790f1ee1acb5431c105cb6cf
Charuru <charuru2009@gmail.com>
2012-11-11 17:29:00 -0500

    include crypto-base as server, do not use crypto-base package

--- package.js
+++ package.js
@@ -3,6 +3,6 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use('crypto-base', ['server']);
+  api.add_files('crypto-base.js', ['server']);
   api.add_files('md5.js', ['server']);
 });
